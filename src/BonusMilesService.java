3ff01441ad78b306d4db9160214799b6447824fe
Nade Korshunova <naanko2021@gmail.com>
2021-11-01 20:28:59 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int ticketPrice){
+        int costOfaMile = 20;
+        int miles = ticketPrice/costOfaMile;
+
+         return miles;
+    }
+
+}
